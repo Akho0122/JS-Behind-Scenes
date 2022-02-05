7fda65e4ac30f1a70ce46853934bf479246fcaba
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-05 18:58:27 +0500

    Java Script Scopes in functions

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2022 - birthYear;
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1991 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = 'Steven';
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-      function add(a, b) {
-        return a + b;
-      }
+//     if (birthYear >= 1991 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = 'Steven';
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      output = 'NEW OUTPUT';
-    }
-    console.log(millenial);
-    // console.log(add(2, 4));
-    console.log(output);
-  }
-  printAge();
-  return age;
+//       output = 'NEW OUTPUT';
+//     }
+//     console.log(millenial);
+//     // console.log(add(2, 4));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+// const firstName = 'Jonas';
+// calcAge(1995);
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
-const firstName = 'Jonas';
-calcAge(1995);
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => {
+  return a + b;
+};
+
+// Example
